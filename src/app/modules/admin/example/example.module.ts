4feedbf51421c31b1472d341d1d1f14702ca7ac4
Vishalkachhadiya <vishal.skillup@gmail.com>
2023-05-29 17:26:30 +0530

    setting-page

--- src/app/modules/admin/example/example.module.ts
+++ src/app/modules/admin/example/example.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatSelectModule } from '@angular/material/select';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatTabsModule } from '@angular/material/tabs';
 import { Route, RouterModule } from '@angular/router';
-import { ExampleComponent } from 'app/modules/admin/example/example.component';
+import { ExampleComponent } from './example.component';
+import { MatMomentDateModule } from '@angular/material-moment-adapter';
+import { CommonModule } from '@angular/common';
 
 const exampleRoutes: Route[] = [
     {
@@ -16,7 +25,16 @@ const exampleRoutes: Route[] = [
     ],
     imports     : [
         RouterModule.forChild(exampleRoutes),
-        MatIconModule
+        CommonModule,
+        MatIconModule,
+        MatInputModule,
+        MatFormFieldModule,
+        MatRadioModule,
+        MatChipsModule,
+        MatDatepickerModule,
+        MatMomentDateModule,
+        MatSelectModule,
+        MatTabsModule,
     ]
 })
 export class ExampleModule{}
