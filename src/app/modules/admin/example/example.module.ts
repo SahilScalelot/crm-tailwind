0b873f9445a7d91d8edd22ea4c491657e306d4ab
uvkyada <udaykyada@gmail.com>
2023-05-29 18:07:43 +0530

    Ex

--- src/app/modules/admin/example/example.module.ts
+++ src/app/modules/admin/example/example.module.ts
@@ -2,6 +2,21 @@ import { NgModule } from '@angular/core';
 import { Route, RouterModule } from '@angular/router';
 import { ExampleComponent } from 'app/modules/admin/example/example.component';
 import { MatIconModule } from '@angular/material/icon';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { CommonModule } from '@angular/common';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatMomentDateModule } from '@angular/material-moment-adapter';
+import { FuseHighlightModule } from '@fuse/components/highlight';
+import { SharedModule } from 'app/shared/shared.module';
+import { MatMenuModule } from '@angular/material/menu';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 
 const exampleRoutes: Route[] = [
     {
@@ -16,7 +31,22 @@ const exampleRoutes: Route[] = [
     ],
     imports     : [
         RouterModule.forChild(exampleRoutes),
-        MatIconModule
+        CommonModule,
+        MatIconModule,
+        MatDatepickerModule,
+        MatRadioModule,
+        MatInputModule,
+        MatFormFieldModule,
+        MatSelectModule,
+        MatButtonModule,
+        MatButtonToggleModule,
+        MatSlideToggleModule,
+        MatChipsModule,
+        MatDividerModule,
+        MatMenuModule,
+        MatMomentDateModule,
+        FuseHighlightModule,
+        SharedModule
     ]
 })
 export class ExampleModule
